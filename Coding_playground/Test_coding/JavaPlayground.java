9fd87c6b934aa7ec7d37dfffa9afb71a259a685d
kskolos <kskolos@cox.net>
2024-08-30 14:02:16 +0200

    I wrote my first line of code to publish on GitHub

---
+++ Coding_playground/Test_coding/JavaPlayground.java
@@ -0,0 +1,7 @@
+package Coding_playground.Test_coding;
+
+public class JavaPlayground {
+    public static void main(String[] args) {
+        System.out.println("Here is my first line of code on GitHub");
+    }
+}
