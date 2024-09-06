b887342371619c0147c7ac1f657ea17f83b12835
kskolos <kskolos@cox.net>
2024-09-06 09:55:54 -0700

    I took away the coding playground test.

--- Coding_playground/Test_coding/JavaPlayground.java
+++
@@ -1,7 +0,0 @@
-package Coding_playground.Test_coding;
-
-public class JavaPlayground {
-    public static void main(String[] args) {
-        System.out.println("Here is my first line of code on GitHub");
-    }
-}
