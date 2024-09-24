62008bd1c95786b67c8202e2aa0f99ff0494c05b
kskolos <kskolos@cox.net>
2024-09-24 16:10:29 -0700

    moving files

---
+++ Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    String title;
+    int year;
+
+    Book() {
+        title = "default";
+        year = 0000;
+    }
+
+
+
+public static void main(String[] args) {
+    Book book = new Book();
+    System.out.println(book.title);
+    }
+}
