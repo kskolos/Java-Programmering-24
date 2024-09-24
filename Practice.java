108c56d87af78f3b6737876f60e5dff0eaccc14f
kskolos <kskolos@cox.net>
2024-09-24 13:17:27 -0700

    practiced creating objects and methods

--- Practice.java
+++ Practice.java
@@ -2,11 +2,6 @@
 
 public class Practice {
     public static void main(String[] args) {
-        int result = 0, a = 0;
-        while (result <= 100 & a <= 10) {
-            result = result + a;
-            a = a + 3;
-            System.out.println();
-        }    
+       
     }
 }
